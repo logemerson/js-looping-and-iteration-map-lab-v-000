9640ec84dae2ebfda2d961358b011f2acd230229
Logan Emerson <logemerson@gmail.com>
2019-07-01 12:32:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
+// `lowerCaseDrivers` - this function takes an array of drivers, and returns an array of the drivers names in lowercase.
+
 function lowerCaseDrivers(drivers) {
   return drivers.map(function(driver) {
     return driver.toLowerCase();
-  });
+  })
 }
+
+//`nameToAttributes` - this function takes an array of drivers with their first and last name separated by a space, and returns an array of JavaScript objects with `firstName` and `lastName` attributes.
 
 function nameToAttributes(drivers) {
-  return drivers.map(function(name) {
-    let nameSplit = name.split(" ");
-    return { firstName: nameSplit[0], lastName: nameSplit[1] };
-  });
+  const driverArr = [];
+
+  return drivers.map(function(driver) {
+    const fName = driver[0];
+    const lName = driver[1];
+    const newDriver = {}
+    
+    newDriver['firstName'] = fName;
+    newDriver['lastName'] = lName;
+    driverArr.push(newDriver);
+  })
+
+  return driverArr;
 }
 
-function attributesToPhrase(drivers) {
-  return drivers.map(function(driver) {
-    return `${driver.name} is from ${driver.hometown}`;
-  });
-}
+
+// `attributesToPhrase` - this function takes an array of drivers as JavaScript objects and returns a string saying `"<NAME OF DRIVER> is from hometown"` for each JavaScript object in the array.
+
