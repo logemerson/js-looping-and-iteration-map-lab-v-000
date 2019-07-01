67b7b6241dd0066cc8eb8f52f91f59b424ff61cc
Logan Emerson <logemerson@gmail.com>
2019-07-01 13:04:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ function nameToAttributes(drivers) {
 
     driverObj['firstName'] = splitDriverArr[0];
     driverObj['lastName'] = splitDriverArr[1];
+  debugger;
 
     driverObjsArr.push(driverObj);
   })
-  debugger;
   console.log('driverObjsArr = ...')
   console.log(driverObjsArr);
   return driverObjsArr;
